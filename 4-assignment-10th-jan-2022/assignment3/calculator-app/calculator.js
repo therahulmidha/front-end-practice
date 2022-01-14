cbd596133f72423a1d0a43798227311de18ad3e0
therahulmidha <rahul.midha31@gmail.com>
2022-01-14 23:10:07 +0530

    Used separate functions for AC and equals

--- 4-assignment-10th-jan-2022/assignment3/calculator-app/calculator.js
+++ 4-assignment-10th-jan-2022/assignment3/calculator-app/calculator.js
@@ -3,14 +3,16 @@ const outputArea = document.querySelector("#output > textarea");
 
 let currentCalculation = "";
 function processBtnPress(value) {
-  if (value === "AC") {
-    currentCalculation = "";
-    outputArea.textContent = 0;
-  } else if (value === "=") {
-    outputArea.textContent = eval(currentCalculation);
-    currentCalculation = outputArea.textContent;
-  } else {
-    currentCalculation += value;
-    outputArea.textContent = currentCalculation;
-  }
+  currentCalculation += value;
+  outputArea.textContent = currentCalculation;
+}
+
+function processACPress() {
+  currentCalculation = "";
+  outputArea.textContent = 0;
+}
+
+function processEqualsPress() {
+  outputArea.textContent = eval(currentCalculation);
+  currentCalculation = outputArea.textContent;
 }
